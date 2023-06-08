ccbe1557e895200ad8b0b6d305108aa8db99b315
orrtaler <74877044+orrtaler@users.noreply.github.com>
2023-06-08 10:10:35 +0300

    more HW

---
+++ src/comps/formColor.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class FormColor extends Component {
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+
