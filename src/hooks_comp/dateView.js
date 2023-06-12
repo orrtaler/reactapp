a2938c998b86b75be76b1bc3c10daf8e24f16729
orrtaler <74877044+orrtaler@users.noreply.github.com>
2023-06-12 13:18:19 +0300

    learn

--- src/hooks_comp/dateView.js
+++ src/hooks_comp/dateView.js
@@ -1,23 +1,36 @@
-import React, { useState } from 'react'
-import { useEffect } from 'react';
+import React, { useEffect, useState } from 'react'
 
+// props -> אם נרצה להביא מהאבא פרופס נצטרך להעביר
+// לפונקציה שלה הקומפנינטה פרמטר בשם פרופס
 export default function DateView(props) {
-    const [days, setDays] = useState(999);
+  const [days,setDays] = useState(999);
 
-    useEffect(() => {
-        calcDays(props.deadline);
-    },[props.deadline])
+  //useEffect
+  // [] -> מערך ריק מקביל ל
+  // componentDidMount - פועל פעם אחת שהקומפ מופיע
+  // [props.deadline] -> מקביל ל
+  // componentDidUpdate -> כאשר יש שינוי בפרופס של הדידליין
+  // מהאבא
+  useEffect(() => {
+    calcDays(props.deadline);
+  },[props.deadline])
 
-    const calcDays = (_newDate) => {
-        let time = Date.parse(_newDate) - Date.now();
-        let newDays = Math.ceil(time / (1000*60*60*24))
-        setDays(newDays);
-    }
 
+//   useEffect(() => {
+//  calcDays(props.deadline)
+//   },[])
+
+
+  const calcDays = (_newDate) => {
+    let time = Date.parse(_newDate) - Date.now();
+    let newDays = Math.ceil(time/(1000*60*60*24))
+    setDays(newDays);
+  }
+ 
   return (
     <div className='text-center'>
-    <h2>CountDown to: {props.deadline}</h2>
-    <h3>days left: {days}</h3>
+      <h2>Countdown to: {props.deadline}</h2>
+      <h3>Days left: {days}</h3>
     </div>
   )
 }
