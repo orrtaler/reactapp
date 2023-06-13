7c54f43cbc075e45dfb293e9d0084a8f8185979e
orrtaler <74877044+orrtaler@users.noreply.github.com>
2023-06-13 09:34:36 +0300

    learn

---
+++ src/comps_hw_learn/app_hw_learn.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import VipList from './vipList'
+
+export default function App_hw_learn(props) {
+  return (
+    <React.Fragment>
+        <VipList/>
+    </React.Fragment>
+  )
+}
