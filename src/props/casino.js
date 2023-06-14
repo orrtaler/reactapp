1c3654cd22897419e901b5846d8c72f09140b8fb
orrtaler <74877044+orrtaler@users.noreply.github.com>
2023-06-14 09:48:58 +0300

    hw

---
+++ src/props/casino.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useState } from 'react'
+
+export default function Casino() {
+
+    const [counter] = useState(5);
+    
+  return (
+    // div class="container">
+    // <h1>Player</h1>
+    // <img src="images/dice1.jpg" alt="dice">
+    // <h2 id="id_h2"></h2>
+    // <h3>coins: <span id="id_span"></span></h3>
+    // <button onclick="roll()" class="btn btn-info">roll</button>
+    // </div>
+  )
+}
