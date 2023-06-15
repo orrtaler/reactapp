50495a977cf53b0fb91e308d3257db4ed766f9b3
orrtaler <74877044+orrtaler@users.noreply.github.com>
2023-06-15 13:15:55 +0300

    hw

--- src/props/casino.js
+++ src/props/casino.js
@@ -5,40 +5,40 @@ export default function Casino() {
 
   const color_ar = ["green", "red"];
   const ans_ar = ["you win ", "you lose"];
+  const img_ar = ["dice1.jpg", "dice2.jpg", "dice3.jpg", "dice4.jpg", "dice5.jpg", "dice6.jpg"]
 
   const [counter, setCounter] = useState(5);
-  const [winOrLose, setWinOrLose] = useState(1);
+  const [winOrLose, setWinOrLose] = useState(0);
+  const [rnd, setRnd] = useState(0);
 
   const rollDice = () => {
-    if(counter > 0){
-      let rnd=Math.random()*6;
-      rnd=Math.ceil(rnd);
-      if(rnd==5||rnd==6){
-        setCounter(counter+1);
-        // setCounter((counter) => counter + 1);
-        setWinOrLose(winOrLose=0);
+    if (counter > 0) {
+      let rnd1 = Math.random() * 6;
+      rnd1 = Math.ceil(rnd1);
+      setRnd(rnd1);
+      if (rnd == 5 || rnd == 6) {
+        setCounter(counter + 1);
+        setWinOrLose(1);
       }
-      else{
-        setCounter(counter-1);
-        // setCounter((counter) => counter - 1);
-        setWinOrLose(winOrLose=1);
+      else {
+        setCounter(counter - 1);
+        setWinOrLose(2);
       }
 
-      // document.querySelector("#id_img").src=`dice_images/dice${rnd}.jpg`;
-      
-  }else{
+    } else {
       alert("you need give money from the bank");
-      setCounter(counter=0);
+      setCounter(1);
+    }
   }
-}
 
   return (
-   <div className="container">
-    <h2>coins: {counter}</h2>
-    {/* <img id="id_img" src="dice_images/dice1.jpg" alt="dice" width="100"> */}
-    {/* <br> */}
-   <h2 style={{color: color_ar[winOrLose]}}>{ans_ar[winOrLose]}</h2>
-    <button className=" btn btn-info" onClick={rollDice}>roll</button>
-</div>
+    <div className="container">
+      <h2>coins: {counter}</h2>
+      <img src={"/images/" + img_ar[rnd]} width="200" height="200" />
+      {/* <img id="id_img" src="dice_images/dice1.jpg" alt="dice" width="100"> */}
+      {/* <br> */}
+      <h2 style={{ color: color_ar[winOrLose] }}>{ans_ar[winOrLose]}</h2>
+      <button className=" btn btn-info" onClick={rollDice}>roll</button>
+    </div>
   )
 }
